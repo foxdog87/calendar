c76ae73b317710804c6f37460a03036ab56e505f
foxdog87 <agiat.0316@gmail.com>
2026-05-22 16:54:34 +0900

    TagCustomFieldを追加

--- app/src/main/java/com/example/calendar/data/entity/Tag.kt
+++ app/src/main/java/com/example/calendar/data/entity/Tag.kt
@@ -2,8 +2,7 @@ package com.example.calendar.data.entity
 
 data class Tag(
     val tagId: Int = 0,         // PK
-    val name: String,           // タグ名（例：「プロジェクトA」）
+    val name: String,          
     val icon: String? = null,
     val color: Int,
-    val customFieldLabel: String? = null // ★ statusから変更：追加したい入力欄の名前が入る
 )
---
+++ app/src/main/java/com/example/calendar/data/entity/TagCustomField.kt
@@ -0,0 +1,7 @@
+package com.example.calendar.data.entity
+
+data class TagCustomField(
+    val fieldId : Int = 0,
+    val tagId : Int,
+    val fieldName : String
+)
