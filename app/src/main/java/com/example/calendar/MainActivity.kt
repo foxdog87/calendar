d1f7f53849d14917d1390c0b1199c2b29634a84a
fox_dog87 <agiat.0316@gmail.com>
2026-06-01 20:34:30 +0900

    タスク生成処理を作成　次回はカレンダー上での表示など

--- app/src/main/java/com/example/calendar/MainActivity.kt
+++ app/src/main/java/com/example/calendar/MainActivity.kt
@@ -30,12 +30,9 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-// ==========================================
-// ★修正：新設計（LocalDateTime）に合わせて、プレビュー用のインスタンス生成を修正
-// ==========================================
+
 private val previewViewModelInstance = CalendarViewModel(
     initialMonth = YearMonth.of(2026, 5),
-    // initialDate ではなく、日付と時間をガッチャンコした initialDateTime を渡します
     initialDateTime = java.time.LocalDateTime.of(2026, 5, 1, 10, 0) // 2026年5月1日 10:00 のダミーデータ
 )
 
